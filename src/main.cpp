9b55cae3fb479b0f15ec8a90e7e343a43725870b
Felix Droop <felix.droop@fu-berlin.de>
2024-02-12 16:06:30 +0100

    filter out useless hits

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #include <pex.hpp>
 #include <search.hpp>
 
+#include <algorithm>
+#include <unordered_map>
+
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
 #include <fmindex-collection/BiFMIndex.h>
 #include <fmindex-collection/occtable/InterleavedEPRV2.h>
-#include <fmindex-collection/search/SearchNg22.h>
+#include <fmindex-collection/search/SearchNg21.h>
 
 int main(int argc, char** argv) {
     auto const opt = cli::parse_options(argc, argv);
@@ -27,6 +30,7 @@ int main(int argc, char** argv) {
         input.reference_sequences, sampling_rate, num_threads
     );
 
+    // for now assume every leaf has the same number of errors
     search_scheme_cache scheme_cache(opt.pex_leaf_num_errors);
     pex_tree_cache tree_cache{};
 
@@ -38,25 +42,48 @@ int main(int argc, char** argv) {
         };
 
         auto const& pex_tree = tree_cache.get(tree_config);
+        // for now assume every leaf has the same length
         auto const& search_scheme = scheme_cache.get(pex_tree.leaf_query_length());
         auto const leaf_queries = pex_tree.generate_leaf_queries(query.sequence);
 
-        fmt::print(
-            "queries:\n{}\nquery length: {}\n", 
-            leaf_queries, 
-            pex_tree.leaf_query_length()
-        );
+        std::vector<std::unordered_map<size_t, size_t>> useful_hits(leaf_queries.size());
 
-        fmindex_collection::search_ng22::search(
-            index, leaf_queries, search_scheme, [&index] (size_t const query_id, auto cursor, size_t const errors, auto const& thing) {
-                fmt::println("found leaf {}, {} times, {} errors, alignment: {}", query_id, cursor.count(), errors, thing);
-                
-                for (auto i{begin(cursor)}; i < end(cursor); ++i) {
-                    auto const [reference_id, pos] = index.locate(i);
-                    fmt::println("- reference {}, position {}", reference_id, pos);
+        fmindex_collection::search_ng21::search(
+            index, leaf_queries, search_scheme, [&index, &useful_hits] (size_t const query_id, auto cursor, size_t const errors) {                
+                auto & useful_query_hits = useful_hits[query_id];
+
+                for (auto hits{begin(cursor)}; hits < end(cursor); ++hits) {
+                    auto const [reference_id, pos] = index.locate(hits);
+
+                    size_t const start = errors <= pos ? pos - errors : 0ul; // protect against underflow
+                    size_t const end = pos + errors;
+                    bool better_hit_exists = false;
+                    for (size_t neighborhood_pos = start; neighborhood_pos <= end; ++neighborhood_pos) {
+                        auto const iter = useful_query_hits.find(neighborhood_pos);
+                        if (iter != useful_query_hits.end() && iter->second < errors) {
+                            better_hit_exists = true;
+                            break;
+                        }
+                    }
+
+                    // if we can't assume that the hits are reported in asceding order of number of errors,
+                    // some cleanup needs to be done here or later
+
+                    if (better_hit_exists) {
+                        continue;
+                    }
+
+                    useful_query_hits[pos] = errors;
                 }
             }
         );
+
+        for (size_t i = 0; i < leaf_queries.size(); ++i) {
+            fmt::println("query: {}", leaf_queries[i]);
+            for (auto const & entry : useful_hits[i]) {
+                fmt::println("    - {}, {} error{}", entry.first, entry.second, entry.second == 1 ? "" : "s");
+            }
+        }
     }
 
     return 0;
