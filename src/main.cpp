13890964ca2b757de412d8f3cebf3b9b23da06d8
Felix Droop <felix.droop@fu-berlin.de>
2024-02-15 17:49:43 +0100

    [FIX/REFACTOR] change and finalize useless hit filtering

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,51 @@
 
 #include <fmindex-collection/search/SearchNg21.h>
 
-bool better_hit_exists_at(
-    size_t const pos, 
-    size_t const threshold,
-    std::unordered_map<size_t, size_t> & hits
-) {
-    auto const iter = hits.find(pos);
-    return iter != hits.end() && iter->second <= threshold;
+struct hit {
+    size_t position;
+    size_t num_errors;
+
+    bool is_better_than(hit const& other) {
+        size_t const position_difference = position < other.position ?
+            other.position - position : position - other.position;
+
+        return num_errors <= other.num_errors && 
+            position_difference <= other.num_errors - num_errors;
+    }
+};
+
+// REFACTOR LATER hits[leaf_query_id][reference_id] -> hits
+using hit_list = std::vector<std::vector<std::vector<hit>>>;
+
+void erase_useless_hits(hit_list & hits) {
+    static constexpr size_t erase_marker = std::numeric_limits<size_t>::max();
+    
+    for (auto & query_hits : hits) {
+        for (auto & query_to_ref_hits : query_hits) {
+            // this must stay, otherwise the expression in the below for loop head could underflow
+            if (query_to_ref_hits.empty()) {
+                continue;
+            }
+
+            std::ranges::sort(query_to_ref_hits, {}, [] (hit const& h) { return h.position; });
+
+            for (size_t i = 0; i < query_to_ref_hits.size() - 1; ++i) {
+                auto & current_hit = query_to_ref_hits[i];
+                size_t j = i + 1;
+
+                while (j < query_to_ref_hits.size() && current_hit.is_better_than(query_to_ref_hits[j])) { 
+                    query_to_ref_hits[j].num_errors = erase_marker;
+                    ++j; 
+                }
+
+                if (j < query_to_ref_hits.size() && query_to_ref_hits[j].is_better_than(current_hit)) {
+                    current_hit.num_errors = erase_marker;
+                }
+            }
+
+            std::erase_if(query_to_ref_hits, [] (hit const& h) { return h.num_errors == erase_marker; } );
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -60,17 +98,17 @@ int main(int argc, char** argv) {
 
     size_t const num_reference_sequences = index_and_data.reference_tags.size();
     auto const& index = index_and_data.index;
-    auto const queries = io::read_queries(opt.queries);
+    auto const fastq_queries = io::read_queries(opt.queries);
 
     // FIX LATER for now assume every leaf has the same number of errors
     search_scheme_cache scheme_cache(opt.pex_leaf_num_errors);
     pex_tree_cache tree_cache{};
 
-    for (auto const& query : queries) {
-        fmt::println("query {}:", query.tag);
+    for (auto const& fastq_query : fastq_queries) {
+        fmt::println("query {}:", fastq_query.tag);
 
         auto const tree_config = pex_tree_config {
-            .total_query_length = query.sequence.size(),
+            .total_query_length = fastq_query.sequence.size(),
             .query_num_errors = opt.query_num_errors,
             .leaf_num_errors = opt.pex_leaf_num_errors
         };
@@ -78,61 +116,39 @@ int main(int argc, char** argv) {
         auto const& pex_tree = tree_cache.get(tree_config);
         // FIX LATER for now assume every leaf has the same length
         auto const& search_scheme = scheme_cache.get(pex_tree.leaf_query_length());
-        auto const leaf_queries = pex_tree.generate_leaf_queries(query.sequence);
+        auto const leaf_queries = pex_tree.generate_leaf_queries(fastq_query.sequence);
         
-        // REFACTOR LATER useful_hits[query_id][reference_id][hit_pos] -> num_errors
-        using hit_map = std::vector<std::vector<std::unordered_map<size_t, size_t>>>;
-        hit_map useful_hits(
+        hit_list hits(
             leaf_queries.size(), 
-            std::vector<std::unordered_map<size_t, size_t>>(num_reference_sequences)
+            std::vector<std::vector<hit>>(num_reference_sequences)
         );
 
         fmindex_collection::search_ng21::search(
             index,
             leaf_queries,
             search_scheme,
-            [&index, &useful_hits] (size_t const query_id, auto cursor, size_t const errors) {                
-                auto & useful_query_hits = useful_hits[query_id];
+            [&index, &hits] (size_t const leaf_query_id, auto cursor, size_t const errors) {                
+                auto & query_hits = hits[leaf_query_id];
 
                 for (auto hit{begin(cursor)}; hit < end(cursor); ++hit) {
                     auto const [reference_id, pos] = index.locate(hit);
-                    auto & useful_query_to_reference_hits = useful_query_hits[reference_id];
-
-                    for (size_t dist = errors; dist != std::numeric_limits<size_t>::max(); --dist) {
-                        size_t const threshold = errors - dist;
-                        size_t const upper_pos = pos + dist;
-                        if (better_hit_exists_at(upper_pos, threshold, useful_query_to_reference_hits)) {
-                            break;
-                        }
-
-                        // this if is only true if we checked all of the possible better hits
-                        // hence we insert now and break to not do the repetitive check
-                        if (dist == 0) {
-                            // FIX LATER if we can't assume that the hits are reported in asceding order of 
-                            // number of errors, some cleanup needs to be done here or later
-                            useful_query_to_reference_hits[pos] = errors;
-                            break;
-                        }
-
-                        size_t const lower_pos = dist <= pos ? pos - dist : 0ul; // protect against underflow
-                        if (better_hit_exists_at(lower_pos, threshold, useful_query_to_reference_hits)) {
-                            break;
-                        }
-                    }
+                    query_hits[reference_id].emplace_back(pos, errors);
                 }
             }
         );
 
-        for (size_t query_id = 0; query_id < leaf_queries.size(); ++query_id) {
-            fmt::println("    leaf: {}", leaf_queries[query_id]);
+        erase_useless_hits(hits);
+
+        for (size_t leaf_query_id = 0; leaf_query_id < leaf_queries.size(); ++leaf_query_id) {
+            fmt::println("    leaf: {}", leaf_queries[leaf_query_id]);
             for (size_t reference_id = 0; reference_id < num_reference_sequences; ++ reference_id) {
-                for (auto const & entry : useful_hits[query_id][reference_id]) {
+                for (auto const & hit : hits[leaf_query_id][reference_id]) {
                     fmt::println(
                         "        - at {}, reference: {}, {} error{}", 
-                        entry.first,
+                        hit.position,
                         index_and_data.reference_tags[reference_id],
-                        entry.second,
-                        entry.second == 1 ? "" : "s"
+                        hit.num_errors,
+                        hit.num_errors == 1 ? "" : "s"
                     );
                 }
             }
