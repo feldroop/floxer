479b262d77ec491d719f7bd3135aded4ca0a3ab6
Felix Droop <felix.droop@fu-berlin.de>
2024-03-14 12:12:20 +0100

    fix: grammar error in floxer description

--- include/about_floxer.hpp
+++ include/about_floxer.hpp
@@ -6,7 +6,7 @@ namespace about_floxer {
 static constexpr std::string version = "0.1.0";
 static constexpr std::string version_date = "09.03.2024";
 static const std::string long_description = 
-    "floxer is exact longread aligner using FM-index search with optimal search schemes, "
+    "floxer is an exact longread aligner using FM-index search with optimal search schemes, "
     "the PEX hierarchical verification scheme and a highly parallel alignment implementation.";
 static const std::string url = "https://github.com/feldroop/floxer";
 
