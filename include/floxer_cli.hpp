64f736eb73d07cdb1d6416308db8b32599d452be
Felix Droop <felix.droop@fu-berlin.de>
2024-06-19 21:39:16 +0200

    change anchor group order default to hybrid

--- include/floxer_cli.hpp
+++ include/floxer_cli.hpp
@@ -44,7 +44,7 @@ class command_line_input {
     cli_option<double> query_error_probability_{ 'p', "error-probability", NAN };
     cli_option<size_t> pex_seed_num_errors_{ 's', "seed-errors", 2 };
     cli_option<size_t> max_num_raw_anchors_{ 'm', "max-anchors", 1'000 };
-    cli_option<std::string> anchor_group_order_{ 'g', "anchor-group-order", "errors_first" };
+    cli_option<std::string> anchor_group_order_{ 'g', "anchor-group-order", "hybrid" };
     cli_option<bool> bottom_up_pex_tree_building_{ 'b', "bottom-up-pex-tree", false };
     cli_option<bool> use_interval_optimization_{ 'n', "interval-optimization", false };
 
